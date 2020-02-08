5f8b9526c7d1b0de5c5c4802b5f37abf659aa57f
amidmajd <amidmajd@gmail.com>
2020-02-08 21:00:55 +0330

    typo

--- friends_rand.py
+++ friends_rand.py
@@ -4,9 +4,9 @@ import re
 import random
 
 
-friends_base_dir = os.path.abspath("/media/amid/AmiD/Video/Friends")
+friends_base_dir = os.path.abspath("/home/amid/Videos/# Series/Friends/")
 print(friends_base_dir)
-ignore_phrases = ["other", 'srt']
+ignore_phrases = ["zOther", 'srt']
 player_command = "vlc"
 
 friends_list = []
@@ -18,5 +18,5 @@ for root, dirs, files in os.walk(friends_base_dir):
             friends_list.append(abs_file_path)
 
 random.shuffle(friends_list)
-# print(friends_list)
-os.system("{} '{}'".format(player_command, friends_list[0]))
+# print(friends_list[0])
+os.system('{} "{}"'.format(player_command, friends_list[0]))
