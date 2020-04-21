bf9313b4a695d2f9546c68f9b525a433688d1891
Amid <amidmajd@gmail.com>
2020-04-21 12:58:34 +0430

    random fixed

--- friends_rand.py
+++ friends_rand.py
@@ -3,9 +3,9 @@ import sys
 import re
 import random
 
-
+random.seed(os.urandom(1024))
 friends_base_dir = os.path.abspath("/home/amid/Videos/# Series/Friends/")
-print(friends_base_dir)
+print('Random based on This Path:', friends_base_dir)
 ignore_phrases = ["zOther", 'srt']
 player_command = "vlc"
 
@@ -14,9 +14,7 @@ for root, dirs, files in os.walk(friends_base_dir):
     for name in files:
         abs_file_path = os.path.join(root, name)
         if re.findall(pattern="|".join(ignore_phrases), string=abs_file_path) == []:
-            # print(abs_file_path)
             friends_list.append(abs_file_path)
 
 random.shuffle(friends_list)
-# print(friends_list[0])
 os.system('{} "{}"'.format(player_command, friends_list[0]))
